2db751521e981e1a637ac1c655d99e1712f859d9
Arda <arda.bakici04@gmail.com>
2021-06-01 00:01:46 +0300

    test bug

--- test_get.py
+++ test_get.py
@@ -2,7 +2,5 @@ import pytest
 from flask import url_for, request
 
 def test_homepage_get(client):
-    test_client = client.test_client()
-    with test_client:
-        res = test_client.get(url_for('/'), follow_redirects=True)
-        assert res.status_code == 200
+    res = client.get(url_for('/'), follow_redirects=True)
+    assert res.status_code == 200
