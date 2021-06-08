d673897bcc52dc5ee615a540d2ccf84b69144ce2
Arda <arda.bakici04@gmail.com>
2021-06-08 12:18:38 +0300

    openssl version print

--- app.py
+++ app.py
@@ -172,6 +172,7 @@ def getRecomendedLot():
         return least_traffic_park["park"]
 
 def send_email(message):
+    print(ssl.OPENSSL_VERSION)
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     with smtplib.SMTP("us2.smtp.mailhostbox.com", port) as server:
         server.starttls(context=context)
