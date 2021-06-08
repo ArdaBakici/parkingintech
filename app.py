f843981fb1b96dd11bee6523e7ed16c63d2eb5b2
Arda <arda.bakici04@gmail.com>
2021-06-08 12:45:33 +0300

    d

--- app.py
+++ app.py
@@ -173,8 +173,8 @@ def getRecomendedLot():
 
 def send_email(message):
     print(ssl.OPENSSL_VERSION)
-    print(ssl.HAS_TLSv1)
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    print(ssl.HAS_TLSv1_1)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_1)
     with smtplib.SMTP("us2.smtp.mailhostbox.com", port) as server:
         server.starttls(context=context)
         server.login("info@parking-in.tech", "CElqRZc2")
